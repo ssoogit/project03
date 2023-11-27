dd7ca0620b7a0bf016b307fc7b2ce9af374bce80
ssoogit <ssoogit02@gmail.com>
2023-11-28 00:04:52 +0900

    3주차 참고자료 제작

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<int:content_id>/', views.detail),
+    path('<int:content_id>/', views.detail, name='detail'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import MainContent
 
 def index(request) :
@@ -7,6 +7,6 @@ def index(request) :
     return render(request, 'mysite/content_list.html', context)
 
 def detail(request, content_id):
-    content_list = MainContent.objects.get(id=content_id)
-    context = { 'content_list': content_list}
+    content_list = get_object_or_404(MainContent, pk=content_id)
+    context = {'content_list': content_list}
     return render(request, 'mysite/content_detail.html', context)
